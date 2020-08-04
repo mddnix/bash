a2d90c9c534f5b82f5d7da33607768ecabe8fc1a
Madhusudhan D Desai <mddnix@gmail.com>
2020-08-04 12:03:21 +0530

    Added _is_file dunction

--- xvs.sh
+++ xvs.sh
@@ -1,23 +1,44 @@
 #!/bin/bash
-# Script Name : xvs.sh
+# Script Name : xvs
 # Description : Provides various functions to manipulate a video file.
 # Args        : Refer HELP section for usage.
 # Author      : Madhusudhan D Desai
 # Email       : mddnix@gmail.com
 
+# Bash shell options
+# ------------------
+set -o nounset # Exit when script tries to use undeclared variables.
+set -o errexit # Exit script when command fails. Add '|| true' to allow fail.
 
-##################################################################################
-# Cleans up temporary files from the directory where the script has been executed.
-# Globals:
-#   none
-# Arguments:
-#   none
-##################################################################################
+# Constant variables
+# ------------------
+readonly CURRENT_DIR="$PWD"
+
+
+###############################################################################
+# Delete temp files from the directory where the script has been executed.
+# Globals/Constants:
+#   CURRENT_DIR
+###############################################################################
 _trap_cleanup() {
 	echo "Oops! $(basename $(0)) ($$) has terminated."
 	echo "Cleaning temporary files..."
-	find "$PWD" -type f -name "tmp_xvs_*.mp4" -delete
+	find "${CURRENT_DIR}" -type f -name "tmp_xvs_*.mp4" -print -delete
+	echo "Done"
 	exit 2
 }
 trap '_trap_cleanup' SIGHUP SIGINT SIGQUIT SIGQUIT SIGTERM
+
+
+###############################################################################
+# Checks if file exists.
+# Arguments:
+#   $1: Filename
+###############################################################################
+_is_file() {
+	if [[ ! -f "$1" ]] ; then
+		echo "ERROR: '$1' does not exist"
+		exit 2
+	fi
+}
 
